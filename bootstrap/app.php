bb71c5b75be2df2578fcc4e2a8ea22434bac9da1
tarek <bekkaoui.tarek2001@gmail.com>
2026-04-27 12:22:56 +0100

    fix: call useStoragePath on Application instance after create()

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,17 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__));
-
-// Sur Vercel, /tmp est le seul répertoire writable
-if (env('LARAVEL_STORAGE_PATH')) {
-    $app->useStoragePath(env('LARAVEL_STORAGE_PATH'));
-}
-if (env('LARAVEL_BOOTSTRAP_CACHE')) {
-    $app->useBootstrapPath(env('LARAVEL_BOOTSTRAP_CACHE'));
-}
-
-return $app
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -29,3 +19,13 @@ return $app
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Sur Vercel, /tmp est le seul répertoire writable
+if (env('LARAVEL_STORAGE_PATH')) {
+    $app->useStoragePath(env('LARAVEL_STORAGE_PATH'));
+}
+if (env('LARAVEL_BOOTSTRAP_CACHE')) {
+    $app->useBootstrapPath(env('LARAVEL_BOOTSTRAP_CACHE'));
+}
+
+return $app;
